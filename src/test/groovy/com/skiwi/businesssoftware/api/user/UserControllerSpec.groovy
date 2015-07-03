f73e297c39e582e40acc41d8cf05a7e427fd503a
skiwi <frank_skiwi@hotmail.com>
2015-07-03 19:45:52 +0200

    Added Clock Spring bean injection to the UserController test. Made the create user test stricter, now only one user with the given email is allowed to be created.

--- src/test/groovy/com/skiwi/businesssoftware/api/user/UserControllerSpec.groovy
+++ src/test/groovy/com/skiwi/businesssoftware/api/user/UserControllerSpec.groovy
@@ -9,6 +9,9 @@ import grails.test.mixin.TestMixin
 import grails.test.mixin.domain.DomainClassUnitTestMixin
 import spock.lang.Specification
 
+import java.time.Clock
+import java.time.Instant
+
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
@@ -16,13 +19,19 @@ import spock.lang.Specification
 @Mock([UserService, User, Person])
 @TestMixin(DomainClassUnitTestMixin)
 class UserControllerSpec extends Specification {
+    static doWithSpring = {
+        clock(Clock) { bean ->
+            bean.factoryMethod = "systemDefaultZone"
+        }
+    }
+
     def "create user"() {
         when: "first user created"
         def result = controller.createUser(new CreateUserMessage("John", null, "Doe", "info@doe.com", "johndoe"))
 
         then: "success result and user is created"
         result.success
-        User.countByEmail("info@doe.com")
+        User.countByEmail("info@doe.com") == 1
 
         when: "second user created with same email"
         def result2 = controller.createUser(new CreateUserMessage("Jane", null, "Doe", "info@doe.com", "johndoe"))
